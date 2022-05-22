c7bd6d45e60e39b56c43dcf6837cc9a91dda96f1
julia <hitosanches@hotmail.com>
2022-05-21 23:07:20 -0300

    Projeto 35%

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [job, setJob] = useState([]);
+
+  useEffect(() => {
+    fetch("./data.json", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => setJob(res));
+  }, []);
+
+  // let numero
+  // const result = job.filter((data)=>  {return data.id === numero})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {job.map((result) => (
+        <div key={result.id} className="container">
+          <div className="image-container">
+            <img className="image" src={result.logo} alt="logo" />
+          </div>
+          <div className="main">
+            <p className="company">{result.company}</p>
+            <p className="new">{result.new} </p>
+            <p className="featured">{result.featured}</p>
+            <p className="position">{result.position} </p>
+            <p className="filters">
+              {result.role} {result.level} {result.languages} {result.tools}
+            </p>
+            <p className="subTitle">
+              {result.location} {result.contract} {result.postedAt}
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
-
 export default App;
