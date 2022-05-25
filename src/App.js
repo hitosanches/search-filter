ad96d44cc30c18135cce5e5fce662dd5b6f5818d
Henrique Sanches <hitosanches@hotmail.com>
2022-05-25 17:40:23 -0300

    estilização quase finalizada, falta apenas search-bar e filtro de pesquisa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Header from "./backgrounds/bg-header-desktop.svg"
 
 function App() {
   const [job, setJob] = useState([]);
@@ -17,38 +18,48 @@ function App() {
   
   return (
     <div>
+          <div className="header">
+            <img src={Header} alt="HeaderDesktop" width="100%" />
+            <div className="search-bar">
+              aaaaaaa
+            </div>
+          </div>
       {job.map((result) => (
         <div key={result.id} className="container">
-          <div className="inner-container">
+          <div className={result.featured ? "inner-container-left-bar" : "inner-container" }>
             <div className="image-container">
               <img className="image" src={result.logo} alt="logo" />
             </div>
             <div className="main">
               <div className="main-info-container">
                 <div className="title">
-                  <div className="teste">
+                  <div className="sub-container">
                     <div className="newfeatured">
                       <p className="company">{result.company}</p>
-                      <p>{result.new ? <p className="new">NEW!</p> : ""}</p>
-                      <p>
+                      <div>{result.new ? <p className="new">NEW!</p> : ""}</div>
+
+                      <div>
                         {result.featured ? (
-                          <p className="featured">FEATURED!</p>) : ("")}
-                      </p>
+                          <p className={`featured`}>FEATURED!</p>) : ("")}
+                      </div>
+
                     </div>
                     <p className="position">{result.position} </p>
-                    <p className="subtitle">
+                    <div className="subtitle">
                       <p className="subtitle-dot">{result.postedAt}</p>{" "}
                       <p className="subtitle-dot">{result.contract}</p>{" "}
                       {result.location}
-                    </p>
+                    </div>
                   </div>
                 </div>
               </div>
               <div className="filters-container">
                 <p className="filters">{result.role}</p>
                 <p className="filters">{result.level} </p>
-                <p className="filters">{result.tools}</p>
-                <div className="languages">{result.languages.map((lang) => (<p className="filters">{lang}</p>))} </div>
+                {result.tools.length > 0 && (<p className="filters">{result.tools}</p>)}
+                <div className="languages">
+                {result.languages.map((lang, index) => ( <span className="filters" key={index} >{lang}</span> ))}
+                 </div>
               </div>
             </div>
           </div>
